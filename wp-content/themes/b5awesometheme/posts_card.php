accbc49ced211c13bf1db72a0447a9968aa6a673
Felix Kiptoo <felixkpt@gmail.com>
2022-07-01 09:37:09 +0300

    responsive image set up for posts_card

--- wp-content/themes/b5awesometheme/posts_card.php
+++ wp-content/themes/b5awesometheme/posts_card.php
@@ -4,12 +4,14 @@ while ($i < 1) {
     <div class="col p-2">
 
         <div class="card h-100 card-default">
-            <div class="card border-0 mx-auto overflow-hidden h-100 pt-2">
-                <a class="text-decoration-none overflow-hidden h-100" href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>">
-                    <div class="col-sm-9 col-12 mx-auto">
-                        <img class="rounded-sm" src="<?php echo $image_url ?>" alt="">
-                    </div>
-                </a>
+            <div class="card border-0 mx-auto h-100 pt-2">
+                <div class="col-12 overflow-hidden">
+                    <a class="text-decoration-none overflow-hidden h-100" href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>">
+                        <div class="col-sm-9 col-12 mx-auto">
+                            <img class="rounded-sm" src="<?php echo $image_url ?>" alt="">
+                        </div>
+                    </a>
+                </div>
             </div>
             <div class="col-12 px-2">
                 <a class="d-block link-primary" href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>">
